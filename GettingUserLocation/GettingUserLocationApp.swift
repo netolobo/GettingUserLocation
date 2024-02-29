656e790ca10805883cfa18b7051efa63dc7fe27f
netolobo <desilio@gmail.com>
2024-02-29 06:59:39 -0800

    Initial Commit

---
+++ GettingUserLocation/GettingUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingUserLocationApp.swift
+//  GettingUserLocation
+//
+//  Created by Neto Lobo on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GettingUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
